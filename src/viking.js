c84a02fdaa2a952bd90a63d4f849a982e1c8dee0
Hollie Duncan <holduncan7@gmail.com>
2022-01-21 19:11:25 +0000

    Completed mandetory, still working on bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,80 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength; }
+    attack(){
+      return this.strength;
+    }
+    receiveDamage(damage) {
+      this.health -= damage;
+    }
+  }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name,health,strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return(`${this.name} has received ${damage} points of damage`)
+    } else if (this.health <=0) {
+      return(`${this.name} has died in act of combat`)
+    }}
+    battleCry() {
+      return "Odin Owns You All!"
+    }
+
+  }
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return(`A Saxon has received ${damage} points of damage`)
+    } else if (this.health <=0) {
+      return(`A Saxon has died in combat`);
+    }}
+}
 
 // War
-class War {}
+class War {
+constructor(){
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+  addViking(army) {
+  this.vikingArmy.push(army);
+  }
+  addSaxon(army) {
+  this.saxonArmy.push(army)
+  }
+  vikingAttack() {
+  let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+  let randomViking= this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+  let vikingAttackSaxon = randomSaxon.receiveDamage(randomViking.strength);
+  
+  return vikingAttackSaxon;
+  }
+
+  saxonAttack() {
+  let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+  let randomViking= this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+  let saxonAttackViking = randomViking.receiveDamage(randomSaxon.strength);
+  
+  return saxonAttackViking;
+  
+    
+  }
+  showStatus() {
+
+  }
+}
 
 
 
